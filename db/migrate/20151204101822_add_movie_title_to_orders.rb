960107b0ba53be7d42f0503469be2d7be85594f1
Yifu She <sheyifu1994@gmail.com>
2015-12-04 10:20:40 +0000

    make it better

--- db/migrate/20151204101822_add_movie_title_to_orders.rb
+++ db/migrate/20151204101822_add_movie_title_to_orders.rb
@@ -1,6 +1,5 @@
 class AddMovieTitleToOrders < ActiveRecord::Migration
   def change
     add_column :orders, :movie_title, :string
-    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204020333) do
+ActiveRecord::Schema.define(version: 20151204101822) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 20151204020333) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "movie_title"
-    t.string   "varchar"
   end
 
   create_table "posts", force: :cascade do |t|
